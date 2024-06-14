5b5a13f4a9bbc335a34dcfe4fb36889d449b7e6f
marcosimao12 <marcosimaocarvalho@gmail.com>
2024-06-14 15:20:33 +0100

    fase inicial do projeto

--- backend/app.js
+++ backend/app.js
@@ -1,21 +1,21 @@
+require('dotenv').config();
 const express = require('express')
 const cors = require('cors');
+const {db} = require('./db/db');
 const app = express()
 
 
-require('dotenv').config();
+
 
 const PORT = process.env.PORT
 
 app.use(express.json())
 app.use(cors());
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
 
 
 const server = () => {
+    db()
     app.listen(PORT, () => {
             console.log(`Server is running on port:`, PORT)
     })
---
+++ backend/db/db.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const db = async () => {
+    try {
+         mongoose.set('strictQuery', false)
+         await mongoose.connect(process.env.MONGO_URL)
+         console.log('MongoDB connected')
+    } catch (error) {
+        console.log('MongoDB connection failed');
+    }
+}
+
+module.exports = {db};
+
+
