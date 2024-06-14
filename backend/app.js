7dffecbb83b60e47830894c3f8fef35baf58c416
marcosimao12 <marcosimaocarvalho@gmail.com>
2024-06-14 14:02:46 +0100

    first commit

---
+++ backend/app.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const cors = require('cors');
+const app = express()
+
+
+require('dotenv').config();
+
+const PORT = process.env.PORT
+
+app.use(express.json())
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+
+const server = () => {
+    app.listen(PORT, () => {
+            console.log(`Server is running on port:`, PORT)
+    })
+
+}
+
+server()
